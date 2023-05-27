6bbf495eb0debcdba94ca9e0c6858ccab7dc0a84
Rod224411 <103529855+Rod224411@users.noreply.github.com>
2023-05-27 17:38:48 -0500

    Add files via upload

---
+++ semana9/servicesImplement/DonadorServiceImplement.java
@@ -0,0 +1,35 @@
+package pe.edu.upc.trabajof.servicesimplement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.trabajof.entities.Donador;
+import pe.edu.upc.trabajof.repositories.DonadorRepository;
+import pe.edu.upc.trabajof.services.DonadorService;
+
+import java.util.List;
+
+@Service
+public class DonadorServiceImplement implements DonadorService {
+    @Autowired
+    private DonadorRepository dR;
+
+    @Override
+    public void insert(Donador donador) {
+        dR.save(donador);
+    }
+
+    @Override
+    public List<Donador> list() {
+        return dR.findAll();
+    }
+
+    @Override
+    public void delete(int idDonador) {
+        dR.deleteById(idDonador);
+    }
+
+    @Override
+    public Donador listId(int idDonador) {
+        return dR.findById(idDonador).orElse(new Donador());
+    }
+}
