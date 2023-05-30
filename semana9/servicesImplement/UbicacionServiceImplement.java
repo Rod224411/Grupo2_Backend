29d286ae6a8f5fa9f48a41142ce5382f2391e14e
Victormrl-17 <116858543+Victormrl-17@users.noreply.github.com>
2023-05-29 21:02:46 -0500

    Add files via upload

---
+++ semana9/servicesImplement/UbicacionServiceImplement.java
@@ -0,0 +1,34 @@
+package pe.edu.upc.semana9.servicesImplement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.semana9.entities.Ubicacion;
+import pe.edu.upc.semana9.repositories.IUbicacionRepository;
+import pe.edu.upc.semana9.services.IUbicacionService;
+
+import java.util.List;
+@Service
+public class UbicacionServiceImplement implements IUbicacionService {
+    @Autowired
+    private IUbicacionRepository dR;
+    @Override
+    public void insert(Ubicacion ubicacion) {
+        dR.save(ubicacion);
+    }
+
+    @Override
+    public List<Ubicacion> list() {
+        return dR.findAll();
+    }
+
+    @Override
+    public void delete(int idUbicacion) {
+        dR.deleteById(idUbicacion);
+    }
+
+    @Override
+    public Ubicacion listId(int idUbicacion) {
+        return dR.findById(idUbicacion).orElse(new Ubicacion());
+    }
+}
+}
