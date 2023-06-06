249d587d08dbbb5d1807278eb3433a2946e08e99
Rod224411 <103529855+Rod224411@users.noreply.github.com>
2023-06-05 22:55:48 -0500

    Add files via upload

---
+++ semana9/dtos/DonacionDTO.java
@@ -0,0 +1,64 @@
+package pe.edu.upc.trabajof.dtos;
+
+import pe.edu.upc.trabajof.entities.Donador;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+public class DonacionDTO {
+    private int idDonacion;
+    private String tipo;
+    private String viveres;
+    private float cantidad;
+    private Donador donador;
+    private Caso caso;
+
+    public int getIdDonacion() {
+        return idDonacion;
+    }
+
+    public void setIdDonacion(int idDonacion) {
+        this.idDonacion = idDonacion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getViveres() {
+        return viveres;
+    }
+
+    public void setViveres(String viveres) {
+        this.viveres = viveres;
+    }
+
+    public float getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(float cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Donador getDonador() {
+        return donador;
+    }
+
+    public void setDonador(Donador donador) {
+        this.donador = donador;
+    }
+
+    public Caso getCaso() {
+        return caso;
+    }
+
+    public void setCaso(Caso caso) {
+        this.caso = caso;
+    }
+}
