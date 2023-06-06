b0c852553e19df4f0298e23b78651f52b69d0d45
Rod224411 <103529855+Rod224411@users.noreply.github.com>
2023-06-06 00:46:57 -0500

    Update DonadorRepository.java

--- semana9/repositories/DonadorRepository.java
+++ semana9/repositories/DonadorRepository.java
@@ -1,10 +1,16 @@
 package pe.edu.upc.trabajof.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.trabajof.entities.Donador;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface DonadorRepository extends JpaRepository<Donador, Integer> {
-
+    @Query("from Donador d where d.idDonador=:id")
+        List<Donador> buscarDonador(@Param("id")int id);
 }
