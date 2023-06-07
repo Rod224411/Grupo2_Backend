36291f43bad50bd71013b7029b2074f8d8a4b511
Rod224411 <103529855+Rod224411@users.noreply.github.com>
2023-06-07 18:40:57 -0500

    Add files via upload

---
+++ semana9/dtos/UsuarioDTO.java
@@ -0,0 +1,95 @@
+package pe.edu.upc.trabajof.dtos;
+
+import javax.persistence.Column;
+
+public class UsuarioDTO {
+    private int idUsuario;
+
+    private String username;
+
+    private String password;
+
+    private boolean enabled;
+
+    private String nombres;
+
+    private String apellidos;
+
+    private int edad;
+
+    private int telefono;
+
+    private String correo;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
