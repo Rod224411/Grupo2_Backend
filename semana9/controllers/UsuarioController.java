a9c3d2b1e8b48032154d52af69251eaf2afe4058
Rod224411 <103529855+Rod224411@users.noreply.github.com>
2023-06-07 18:37:19 -0500

    Add files via upload

---
+++ semana9/controllers/UsuarioController.java
@@ -0,0 +1,54 @@
+package pe.edu.upc.trabajof.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import pe.edu.upc.trabajof.dtos.UbicacionDTO;
+import pe.edu.upc.trabajof.dtos.UsuarioDTO;
+import pe.edu.upc.trabajof.entities.Ubicacion;
+import pe.edu.upc.trabajof.entities.Usuario;
+import pe.edu.upc.trabajof.services.IUsuarioService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    @Autowired
+    private IUsuarioService uS;
+    @PostMapping
+    public void insert(@RequestBody UsuarioDTO dto) {
+
+        ModelMapper m = new ModelMapper();
+        Usuario u = m.map(dto, Usuario.class);
+        uS.insert(u);
+    }
+
+    @GetMapping
+    public List<UsuarioDTO> list() {
+        return uS.list().stream().map(x -> {
+            ModelMapper m = new ModelMapper();
+            return m.map(x, UsuarioDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id) {
+        uS.delete(id);
+    }
+
+    @GetMapping("/{id}")
+    public UsuarioDTO listId(@PathVariable("id") Integer id) {
+        ModelMapper m = new ModelMapper();
+        UsuarioDTO dto = m.map(uS.listId(id), UsuarioDTO.class);
+        return dto;
+    }
+    @PutMapping
+    public void goUpdate(@RequestBody UsuarioDTO dto) {
+        ModelMapper m=new ModelMapper();
+        Usuario u=m.map(dto,Usuario.class);
+        uS.insert(u);
+    }
+}
